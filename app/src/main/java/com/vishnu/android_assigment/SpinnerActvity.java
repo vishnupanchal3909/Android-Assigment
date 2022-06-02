98fcda9c017418240bb261b25ff1586c0bcd6e08
vishnupanchal3909 <vishnupanchal3909@gmail.com>
2022-06-02 12:14:46 +0530

    android

--- app/src/main/java/com/vishnu/android_assigment/SpinnerActvity.java
+++ app/src/main/java/com/vishnu/android_assigment/SpinnerActvity.java
@@ -8,47 +8,38 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SpinnerActvity extends AppCompatActivity  {
+public class SpinnerActvity extends AppCompatActivity implements
+        AdapterView.OnItemSelectedListener  {
 
-    Spinner spinner;
-    TextView answer;
+    String[] language = { "Android Programming","Java","Angular JS","HTML"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spinner_actvity);
 
-        // Spinner element
-        spinner =findViewById(R.id.listOflangage);
-        answer=findViewById(R.id.answeroflist);
-        String[] yognan=new String[]{
-                "Select-->","Android Programming","Java","C Programming","Angular JS",
-        };
-        ArrayAdapter arrayAdapter=new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,yognan);
-        spinner.setAdapter(arrayAdapter);
-        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                if(adapterView.getItemAtPosition(i)=="Android Programming"){
-                    answer.setText("Android Programming");
-                }
-                /////////////////////
-                if(adapterView.getItemAtPosition(i)=="Java"){
-                    answer.setText("Java");
-                }
-                /////////////////////////////////
-                if(adapterView.getItemAtPosition(i)=="C Programming"){
-                    answer.setText("C Programming");
-                }
-                //////////////////////////////////
-                if(adapterView.getItemAtPosition(i)=="Angular JS"){
-                    answer.setText("Angular JS");
-                }
-            }
-        });
+        //Getting the instance of Spinner and applying OnItemSelectedListener on it
+        Spinner spin = (Spinner) findViewById(R.id.spinner);
+        spin.setOnItemSelectedListener(this);
 
+        //Creating the ArrayAdapter instance having the country list
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,language);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
+
+    }
+    //Performing action onItemSelected and onNothing selected
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        Toast.makeText(getApplicationContext(),"You have Selected "+language[position] , Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
     }
 }
